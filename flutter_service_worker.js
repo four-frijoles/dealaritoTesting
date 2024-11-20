febb38ab19aa0a0d6f55b0136cfcc6ccbc076e95
Jay Patel <pateljay251001@gmail.com>
2024-11-19 20:37:12 -0500

    Last last

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "2ab12b203f661853fd0b1fbb056e2022
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "668b2ed54a2a704fa7e55599e4cc4e9d",
+"flutter_bootstrap.js": "7a62fa86a475caa1fc28629010346b18",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
